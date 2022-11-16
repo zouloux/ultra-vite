b4962e9c4617d2a816b03857e5d6a98ca698cbf0
Alexis Bouhet <alexis@les-hallucines.studio>
2022-11-16 11:15:14 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# Ultra vite
+
+Simple stack for static websites with :
+- ViteJS
+- Typescript
+- Less for styles
+
+## Usage
+
+#### Clone this repository
+- `git clone https://github.com/zouloux/ultra-vite ultra-vite`
+- `cd ultra-vite`
+ 
+#### Un-sync from git
+- `rm -rf .git`
+> Then re-associate to your GIT repo ...
+
+#### Configure package.json
+In `package.json`
+- Fill `name`
+- Fill `version`
+- Fill `description`
+- Fill `author`
+- Fill `licence`
+
+#### Run in dev with watch
+- `npm run dev`
+
+#### Build for production
+- `npm run build`
---
+++ vite.config.js
@@ -0,0 +1,35 @@
+import { defineConfig } from 'vite'
+import { resolve } from "path"
+
+const appConfig = {
+	inputs: [
+		'src/index.html',
+	],
+	output: 'public/'
+}
+
+export default defineConfig( viteConfig => {
+	return {
+		// Les fichiers sources compilés doivent être dans ce dossier
+		root: resolve("src/"),
+		// Configurer le build
+		build: {
+			outDir: resolve(appConfig.output),
+			assetsDir: "./",
+			rollupOptions: {
+				input: appConfig.inputs.map( e => resolve(e) ),
+			}
+		},
+		// Configurer le pipeline CSS
+		css: {
+			preprocessorOptions: {
+				less: {
+					math: 'always'
+				}
+			}
+		},
+		plugins: [
+			// ...
+		]
+	}
+})
